146ad597d3c12dae316de072656cfc6bfa68eea1
Virgold <virgoanchuks@gmail.com>
2023-04-20 03:18:33 +0100

    changing animation duration

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-const element = document.querySelector('.first-animate');
 
-window.onscroll = function () {
-    myFunction()
-};
+window.onscroll = function() {myFunction()};
 
 function myFunction() {
-    if (document.documentElement.scrollTop > 0.5) {
-        console.log(document.documentElement.scrollTop);
-        element.classList.add('animate__zoomIn');
-    } else {
-        console.log('hello');
-    }
+  if (document.documentElement.scrollTop > 10) {
+    console.log(document.documentElement.scrollTop);
+    document.getElementById("first-animate").classList.add ("animate__zoomIn");
+  }
+  if (document.documentElement.scrollTop > 250) {
+    console.log(document.documentElement.scrollTop);
+    document.getElementById("second-animate").classList.add ("animate__fadeInUpBig");
+    document.getElementById("third-animate").classList.add ("animate__fadeInLeft");
+    document.getElementById("fourth-animate").classList.add ("animate__fadeInRight");
+  }
 }
