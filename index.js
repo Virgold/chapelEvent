b552965db4031199e37c7f2f9cf616d498f29183
Virgold <virgoanchuks@gmail.com>
2023-04-20 02:44:35 +0100

    changing scroll animation period

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const element = document.querySelector('.first-animate');
 
-window.onscroll = function () { myFunction() };
+window.onscroll = function () {
+    myFunction()
+};
 
 function myFunction() {
-    if (document.documentElement.scrollTop > 50) {
+    if (document.documentElement.scrollTop > 10) {
         element.classList.add('animate__animated', 'animate__zoomIn'.replace(/\s+/g, '-'));
     } else {
         console.log('hello');
     }
 }
-// element.addEventListener('scroll', () => {
-//     console.log('hello');
-//     element.classList.add('animate__animated', 'animate__zoomIn'.replace(/\s+/g, '-'));
-// });
