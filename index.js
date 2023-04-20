3c79ee96cb6b0e522016c812bb2d303debb8c76d
Virgold <virgoanchuks@gmail.com>
2023-04-20 02:38:04 +0100

    adding scroll animation

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const element = document.querySelector('.first-animate');
 
+window.onscroll = function () { myFunction() };
 
-element.addEventListener('mousemove', () => {
-    console.log('hello');
-    element.classList.add('animate__animated', 'animate__zoomIn'.replace(/\s+/g, '-'));
-});
+function myFunction() {
+    if (document.documentElement.scrollTop > 50) {
+        element.classList.add('animate__animated', 'animate__zoomIn'.replace(/\s+/g, '-'));
+    } else {
+        console.log('hello');
+    }
+}
+// element.addEventListener('scroll', () => {
+//     console.log('hello');
+//     element.classList.add('animate__animated', 'animate__zoomIn'.replace(/\s+/g, '-'));
+// });
